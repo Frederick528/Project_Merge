29a77b3769d4db720377b69db12372da4a6ccb92
LeiDaSorte <75717568+Frederick528@users.noreply.github.com>
2024-01-09 14:06:11 +0900

    Revert "Revert "Click Effect""
    This reverts commit 154089e254356b4dadf546474bd5893c50596302.

--- Assets/2. Scripts/Particle/Click.cs
+++ Assets/2. Scripts/Particle/Click.cs
@@ -6,10 +6,22 @@ using UnityEngine;
 public class Click : MonoBehaviour
 {
     public Animator anim;
-    // Start is called before the first frame update
+    public RectTransform particleCanvas;
+    Vector2 particleCanvasSize;
 
-    void OnMouseDown()
+
+    private void Start()
     {
-        anim.Play("ClickAnimation", -1, 0);
+        particleCanvasSize = particleCanvas.sizeDelta / 2;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.transform.localPosition = new Vector3(Input.mousePosition.x - particleCanvasSize.x, Input.mousePosition.y - particleCanvasSize.y, 0);
+            anim.Play("ClickAnimation", -1, 0);
+
+        }
     }
 }

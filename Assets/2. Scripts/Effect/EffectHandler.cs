590fbb31b9484fe511e09d1abf806d94e9c77f7a
LeiDaSorte <75717568+Frederick528@users.noreply.github.com>
2024-01-10 17:48:16 +0900

    Card Effect
    카드 생성될 때, 이펙트 실행
    단, 애니메이션이 끝나면 이펙트를 비활성화하기 때문에 오브젝트 풀링으로 코드를 바꿀 경우 초기화 필요.
    + 프리팹에 이펙트를 추가한 것.

---
+++ Assets/2. Scripts/Effect/EffectHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectHandler : MonoBehaviour
+{
+    #region Members
+    private Animator m_Animator;
+
+    #endregion Members
+
+
+    #region Methods
+    void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    public void StartAnimation()
+    {
+
+    }
+
+    public void EndAnimation()
+    {
+        transform.gameObject.SetActive(false);
+    }
+
+    #endregion Methods
+}

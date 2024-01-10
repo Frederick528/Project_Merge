962a51a0ffb7c9e5bda968633c741d022a87b512
LeiDaSorte <75717568+Frederick528@users.noreply.github.com>
2024-01-10 15:53:45 +0900

    간단한 수정
    카드캔버스 초반에 ON 상태
    카드 스크립트에서 아이디 init도 메테리얼 잘 가져오도록 수정

--- Assets/2. Scripts/GameManager.cs
+++ Assets/2. Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    public static bool CardCanvasOn = false;
+    public static bool CardCanvasOn = true;
 
     void Awake()
     {
